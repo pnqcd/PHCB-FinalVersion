699a79094a54193bfb82627f912d06e2e8a7505f
phanthaikhang21127069 <101028729+phanthaikhang21127069@users.noreply.github.com>
2024-01-07 18:58:14 +0700

    change tab to login if reset password success + change email sending style

--- controllers/authController.js
+++ controllers/authController.js
@@ -60,7 +60,7 @@ const generateOTP = (req) => {
 };
 
 controller.forgetPassword = async (req, res) => {
-    const { email} = req.body;
+    const { email } = req.body;
     try {
         // Check if the email exists in the database
         const user = await models.User.findOne({ where: { email } });
@@ -72,6 +72,7 @@ controller.forgetPassword = async (req, res) => {
                 message: "Email không tồn tại trong hệ thống",
             });
         }
+        const usrfullname = user.fullName;
 
         // Generate OTP
         const otp = generateOTP(req);
@@ -100,14 +101,140 @@ controller.forgetPassword = async (req, res) => {
             to: email,
             subject: 'Password Reset OTP',
             html: `
-            <p>You have selected ${email} as your verification OTP to reset the password.</p>
-            <p style ="width: 80px; background-color: #ECECEC; font-size: 20px; font-weight: bold">${otp}</p>
-            <p>The code will expire 1 minute after this email was sent.</p>
-            <p>Thank you.</p>
-            <p>Regard,</p>
-            <p style = "font-weight: bold">Ad Management System</p>
-            <p style = "font-style: italic">"Best investment we made for a long time. Can only recommend it for other users."</p>
-            `,
+            <table width="100%" border="0" cellpadding="0" direction="ltr" bgcolor="#f1f1f1" cellspacing="0" role="presentation" style="width: 640px; min-width: 640px; margin:0 auto 0 auto;">
+<tbody>
+
+<tr>
+	<td>
+		<!--Content Starts Here -->
+		<table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" role="presentation" bgcolor="#f1f1f1">
+		<tr>
+			<td height="30" style="line-height:30px;min-height:30px;">
+			</td>
+		</tr>
+		</table>
+		<!--Top Header Starts Here -->
+		<table border="0" bgcolor="#121212" cellpadding="0" cellspacing="0" width="640" role="presentation" width="640" style="width: 640px; min-width: 640px;" align="center" class="table-container ">
+		<tbody>
+		<tr width="640" style="width: 640px; min-width: 640px; " align="center">
+			<td>
+				<table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" role="presentation" bgcolor="#F4E9D9">
+				<tr>
+					<td height="35" style="line-height:35px;min-height:35px;">
+					</td>
+				</tr>
+				</table>
+				<table cellpadding="0" cellspacing="0" border="0" width="640" style="width: 640px; min-width: 640px;" role="presentation" align="center" bgcolor="#F4E9D9">
+				<tr>
+					<td align="left">
+						<table cellpadding="0" cellspacing="0" border="0" role="presentation" align="center" bgcolor="#F4E9D9">
+						<tr>
+							<td>
+								<table cellpadding="0" cellspacing="0" border="0" align="center" role="presentation">
+								<tr>
+									<td align="center">
+									</td>
+								</tr>
+								</table>
+								<table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" role="presentation" bgcolor="#F4E9D9">
+								<tr>
+									<td height="35" style="line-height:35px;min-height:35px;">
+									</td>
+								</tr>
+								</table>
+							</td>
+						</tr>
+						</table>
+					</td>
+				</tr>
+				</table>
+			</td>
+		</tr>
+		</tbody>
+		</table>
+		<!--Top Header Ends Here -->
+		<table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" role="presentation" bgcolor="#f7f8fb">
+		<tr>
+			<td height="30" style="line-height:30px;min-height:30px;">
+			</td>
+		</tr>
+		</table>
+		<table align="center" border="0" cellpadding="0" cellspacing="0" width="640" role="presentation" bgcolor="#f7f8fb" class="table-container ">
+		<tbody>
+		<tr>
+			<td align="left" style="color:#45535C;padding:20px 40px 0 40px;font-family: 'Lato', Arial, Helvetica, sans-serif;font-weight:800;font-size:34px;-webkit-font-smoothing:antialiased;line-height:1.2;" class="table-container mobile-title">
+				 Mã OTP reset password
+			</td>
+		</tr>
+		<tr>
+			<td align="left" style="color:#5a5a5a;padding:20px 40px 0 40px;font-family: 'Lato', Arial, Helvetica, sans-serif;font-weight:normal;font-size:16px;-webkit-font-smoothing:antialiased;line-height:1.4;" class="table-container">
+			</td>
+		</tr>
+		</tbody>
+		</table>
+		<table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" role="presentation" bgcolor="#f7f8fb">
+		<tr>
+			<td height="60" style="line-height:60px;min-height:60px;">
+			</td>
+		</tr>
+		</table>
+		<table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" role="presentation" bgcolor="#FFFFFF">
+		<tr>
+			<td height="30" style="line-height:30px;min-height:30px;">
+			</td>
+		</tr>
+		</table>
+		<table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" role="presentation" bgcolor="#FFFFFF">
+		<tbody>
+		<tr>
+			<td align="left" style="color:#45535C;padding:20px 40px 0 40px;font-family: 'Lato', Arial, Helvetica, sans-serif;font-weight:800;font-size:24px;-webkit-font-smoothing:antialiased;line-height:1.2;" class="table-container mobile-title">
+				Chào ${usrfullname},
+			</td>
+		</tr>
+		<tr>
+			<td align="left" style="color:#5a5a5a;padding:20px 40px 0 40px;font-family: 'Lato', Arial, Helvetica, sans-serif;font-weight:normal;font-size:14px;-webkit-font-smoothing:antialiased;line-height:1.4;" class="table-container">
+				 Hệ thống Ads Management đã nhận được yêu cầu reset mật khẩu từ ${email} của bạn. 
+                 <br><b>Mã OTP reset mật khẩu:
+                 <div class="grey-text-box" style = "font-size: 28px;
+                 background-color: #f0f0f0; /* Light grey background color */
+                 padding: 10px; /* Adjust the padding as needed */
+                 border: 1px solid #ccc; /* Optional border for the text box */
+                 color: #000; /* Black text color */
+                 width: 220px;
+                 height: 35px;">
+                 ${otp}
+                </div>
+                 
+                 <br><br>Hãy liên hệ với chúng tôi khi bạn gặp khó khăn trong quá trình sử dụng ứng dụng thông qua email: <a href="mailto:ptudw.group.4@gmail.com" target="_blank" class="link t-grey-1" style="text-decoration:underline; color:#4f48e0;">ptudw.group.4@gmail.com
+</a>.
+				<br>Xin cảm ơn bạn đã sử dụng dịch vụ của chúng tôi. Kính chúc bạn có những trải nghiệm tuyệt vời khi thực hiện giao dịch trên hệ thống Ads Management. 
+                 <br><br><br>Trân trọng,
+                 <br>Ads Management.
+                 
+                 <br><br><br><b>Lưu ý:</b> Không chia sẻ mã OTP này cho bất kỳ ai để tránh bị đánh cắp tài khoản. Mã OTP reset mật khẩu này chỉ có hiệu lực trong vòng <b>01 phút</b> kể từ thời điểm nhận được.
+			</td>
+		</tr>
+		</tbody>
+		</table>
+		<table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" role="presentation" bgcolor="#FFFFFF">
+		<tr>
+			<td height="60" style="line-height:60px;min-height:60px;">
+			</td>
+		</tr>
+		</table>
+		
+		<table bgcolor="#f1f1f1" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" role="presentation" class="table-container ">
+		<tr>
+			<td height="35" style="line-height:35px;min-height:35px;">
+			</td>
+		</tr>
+		</table>
+		<table bgcolor="#f1f1f1" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" role="presentation" class="table-container ">
+		<tr>
+			<td height="35" style="line-height:35px;min-height:35px;">
+			</td>
+		</tr>
+		</table>`,
         };
 
         // Send email
@@ -160,21 +287,21 @@ controller.verifyOTP = (req, res) => {
 };
 
 controller.resetPassword = async (req, res) => {
-    let {newPassword} = req.body;
+    let { newPassword } = req.body;
     const userEmail = req.session.email;
 
-    const user = await models.User.findOne({ where: {email: userEmail} });
+    const user = await models.User.findOne({ where: { email: userEmail } });
 
     let hashedPassword = await bcrypt.hash(newPassword, 10);
     try {
         await models.User.update(
-            {password: hashedPassword},
-            {where: {email: userEmail}}
+            { password: hashedPassword },
+            { where: { email: userEmail } }
         );
         res.send("Đã cập nhật mat khau!");
     } catch (error) {
-    res.send("Không thể cập nhật mat khau!");
-    console.error(error);
+        res.send("Không thể cập nhật mat khau!");
+        console.error(error);
     }
 }
 
